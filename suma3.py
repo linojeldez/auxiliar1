cd1f9be134eabbf7c4e6394a7cbc156b89ecf0a8
linojeldez <ljeldezsalinas@gmail.com>
2018-09-25 20:53:08 -0400

    se puede sumar 3 numeros

---
+++ suma3.py
@@ -0,0 +1,4 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 02:47:15) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def suma3(a,b,c):
+	return a + b +c
